f1aff8f414cee3624ed2a627ba4c2cfbf18751ae
GravleyCM <GravleyCM@gmail.com>
2022-02-11 11:24:26 -0500

    add cache element references and set up init funcion

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/ 
-
+let winner, inputValues
 /*------------------------ Cached Element References ------------------------*/
-
+const inputs = document.querySelector("div.input-boxes")
+const storyBtns = document.querySelector("div.button-boxes")
+const resetBtn = document.querySelector(".reset-button")
  /*----------------------------- Event Listeners -----------------------------*/ 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+
+function init() {
+  inputValues = [null, null, null, null, null, null, null, null, null]
+  winner = null
+  // resetBtn.setAttribute("hidden", true)
+render()
+}
+
+function render() {
+
+}
