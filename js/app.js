15679cb1154045ba9db8555f85c72cfdd6bb34fc
GravleyCM <GravleyCM@gmail.com>
2022-02-10 21:07:33 -0500

    add new psuedo file and move pseudocode to new file

--- js/app.js
+++ js/app.js
@@ -1,28 +1,3 @@
-// * Set up basic HTML ouline
-  //* Add inputs for nouns, adjectives, adverbs, etc..
-  //* Create 5 buttons. 4 for separate stories and one for a reset
-
-//* Create function to call an individual story.
-//* Create a function to reset the game
-
-// * Make Cached Element References
-// * Create Event Listeners for buttons
-  //* 
-
-// * Define variable for a win condition
-// * 
-// *
-// *
-// *
-// *
-// *
-// *
-// *
-// *
-// *
-// *
-// *
-
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/ 
