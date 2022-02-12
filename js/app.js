f2e000632725405899c3b19eac0f360a12b702aa
GravleyCM <GravleyCM@gmail.com>
2022-02-12 17:19:45 -0500

    move stories from stories.js to app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 /*-------------------------------- Constants --------------------------------*/
+// let storyOne = `Look, if ${name} had one ${noun}, one opportunity to ${verb} everything ${name} ever wanted - one moment, would ${name} ${verb2} it or just let it ${adv} slip? Yo his palms are ${noun2}, knees weak, ${adj} arms are heavy there's vomit on his ${adj2} sweater already - mom's spaghetti. ${name}'s nervous but on the surface ${name}'s calm and ready to ${adv2} drop bombs but ${name} keeps on forgetting.`
+
+// let storyTwo = `${name} the ${adj} ${noun} was walking up the street to his ${noun2} in Chicago. As ${name}  ${adv} ${verb} inside ${name} saw that all their friends had been replaced by ${adj2} garden gnomes. ${name} was so mad that they ${adv2} ${verb2} into madness and ran into the woods, never to be seen again.`
+
+// let storyThree = `After arriving at the ${adj} goliath village the elders ${adv} squabbled about allowing the ${adj2} ${noun} named ${name} to ${adv} ${verb} as much as they want.  The elders were worried that the sacred ${noun2} would be ${verb2} by the new resident evil of the village.`
+
+// let storyFour = `Imagine for just a second a majestic ${noun} named ${name}. If it could ${adv} ${verb} through the ${adj} ${noun2}, I bet everyone here would ${adv2} ${verb2} as fast as they could back to their homes.`
 
 /*-------------------------------- Variables --------------------------------*/ 
 let winner, inputValues
