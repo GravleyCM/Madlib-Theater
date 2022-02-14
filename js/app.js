8707be2ee64ebc5e35b4864be21713f59b50e69e
GravleyCM <GravleyCM@gmail.com>
2022-02-14 13:40:03 -0500

    fix story 4

--- js/app.js
+++ js/app.js
@@ -23,16 +23,16 @@ storyBtns.addEventListener("click", (evt) => {
     evt.preventDefault()
     let btn = evt.target
     if(btn.id === "story1") {
-      const storyOne = `Look, if ${name.value} had one ${noun.value}, one opportunity to ${verb.value} everything ${name.value} ever wanted - one moment, would ${name.value} ${verb2.value} it or just let it ${adv.value} slip? Yo his palms are ${adj2.value}, knees weak, ${adj.value} arms are heavy there's vomit on his ${noun2.value} sweater already - mom's spaghetti. ${name.value}'s nervous but on the surface ${name.value}'s calm and ready to ${adv2.value} drop bombs but ${name.value} keeps on forgetting.`
+      const storyOne = `Look, if ${name.value.toUpperCase()} had one ${noun.value.toUpperCase()}, one opportunity to ${verb.value.toUpperCase()} everything ${name.value.toUpperCase()} ever wanted - one moment, would ${name.value.toUpperCase()} ${verb2.value.toUpperCase()} it or just let it ${adv.value.toUpperCase()} slip? Yo his palms are ${adj2.value}, knees weak, ${adj.value.toUpperCase()} arms are heavy there's vomit on his ${noun2.value.toUpperCase()} sweater already - mom's spaghetti. ${name.value.toUpperCase()}'s nervous but on the surface ${name.value.toUpperCase()}'s calm and ready to ${adv2.value.toUpperCase()} drop bombs but ${name.value.toUpperCase()} keeps on forgetting.`
       newMadLib.append(`${storyOne}`)
     } else if (btn.id === "story2") {
-      const storyTwo = `${name.value} the ${adj.value} ${noun.value} was walking up the street to his ${noun2.value} in Chicago. As ${name.value}  ${adv.value} ${verb.value} inside ${name.value} saw that all their friends had been replaced by ${adj2.value} garden gnomes. ${name.value} was so mad that they ${adv2.value} ${verb2.value} into madness and ran into the woods, never to be seen again.`
+      const storyTwo = `${name.value.toUpperCase()} the ${adj.value.toUpperCase()} ${noun.value.toUpperCase()} was walking up the street to his ${noun2.value.toUpperCase()} in Chicago. As ${name.value.toUpperCase()}  ${adv.value.toUpperCase()} ${verb.value.toUpperCase()} inside, ${name.value.toUpperCase()} saw that all their friends had been replaced by ${adj2.value.toUpperCase()} garden gnomes. ${name.value.toUpperCase()} was so mad that they ${adv2.value.toUpperCase()} ${verb2.value.toUpperCase()} into madness and ran into the woods, never to be seen again.`
       newMadLib.append(`${storyTwo}`)
     } else if (btn.id === "story3") {
-      const storyThree = `After arriving at the ${adj.value} goliath village the elders ${adv.value} squabbled about allowing the ${adj2.value} ${noun.value} named ${name.value} to ${adv.value} ${verb.value} as much as they want.  The elders were worried that the sacred ${noun2.value} would be ${verb2.value} by the new resident evil of the village.`
+      const storyThree = `After arriving at the ${adj.value.toUpperCase()} goliath village the elders ${adv.value.toUpperCase()} squabbled about allowing the ${adj2.value.toUpperCase()} ${noun.value.toUpperCase()} named ${name.value.toUpperCase()} to ${adv2.value.toUpperCase()} ${verb.value.toUpperCase()} as much as they want.  The elders were worried that the sacred ${noun2.value.toUpperCase()} would be ${verb2.value.toUpperCase()} by the new resident evil of the village.`
       newMadLib.append(`${storyThree}`)
     } else if (btn.id === "story4") {
-      const storyFour = `Imagine for just a second a majestic ${noun.value} named ${name.value}. If it could ${adv.value} ${verb.value} through the ${adj.value} ${noun2.value}, I bet everyone here would ${adv2.value} ${verb2.value} as fast as they could back to their homes.`
+      const storyFour = `Imagine for just a second a ${adj2.value.toUpperCase()} ${noun.value.toUpperCase()} named ${name.value.toUpperCase()}. If it could ${adv.value.toUpperCase()} ${verb.value.toUpperCase()} through the ${adj.value.toUpperCase()} ${noun2.value.toUpperCase()}, I bet everyone here would ${adv2.value.toUpperCase()} ${verb2.value.toUpperCase()} as fast as they could back to their homes.`
       newMadLib.append(`${storyFour}`)
     }
     madForm.setAttribute("hidden", true)
